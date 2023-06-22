9a02b4260e4cd58a036a35e29aa70620b601c98e
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-06-22 19:23:49 +0530

    style: made help responsive

--- src/components/Help.js
+++ src/components/Help.js
@@ -6,7 +6,7 @@ const Section = ({ id, question, answer, isVisible, setIsVisible }) => {
     return (
         <div className='border p-2'>
             <div className='flex justify-between items-center'>
-                <h2 className='font-semibold text-xl'>{question}</h2>
+                <h2 className='font-semibold text-base md:text-xl'>{question}</h2>
                 {
                     id === isVisible ?
                         <AiFillCaretUp className='cursor-pointer' onClick={() => setIsVisible("")} />
@@ -26,7 +26,7 @@ const Help = () => {
     const [isVisible, setIsVisible] = useState("")
     return (
         <>
-            <div className='w-2/3 mx-auto py-32 flex flex-col gap-4'>
+            <div className='md:w-2/3 mx-auto my-32 flex flex-col gap-4 p-3'>
                 {
                     HELP_FAQ.map(faq => <Section key={faq.id} id={faq.id} question={faq.question} answer={faq.answer} isVisible={isVisible} setIsVisible={setIsVisible} />)
                 }
