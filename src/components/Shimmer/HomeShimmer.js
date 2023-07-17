04d8a64e0a4b0c93254a92b536535cb2f0906393
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-07-17 15:51:43 +0530

    remove server issue message

--- src/components/Shimmer/HomeShimmer.js
+++ src/components/Shimmer/HomeShimmer.js
@@ -7,7 +7,6 @@ const HomeShimmer = () => {
             <div className='flex justify-between pt-28 px-16 pb-6'>
                 <div className='h-8 bg-gray-200 w-60'></div>
             </div>
-            <p className='px-4 text-center'>It may take some time due to server inactivity, please check out other pages till then😊</p>
             <hr />
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-20 p-16'>
                 {
