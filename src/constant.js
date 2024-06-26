173908f42a386a270d8397aba1ffe452fd692813
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-06-27 01:09:38 +0530

    chore: Update CDN image URL

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
 export const CDN_IMG_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const HELP_FAQ = [
   {
     id: "first",
